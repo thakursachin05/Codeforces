3bd2f3f6ea70afd59e0494c9ebc819b39ac1eacb
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2023-02-04 22:08:24 +0530

    Added code

---
+++ Introductory Problems/702A-MaximumIncrease.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int arr[n+1];
+    for(int i=1;i<=n;i++) cin>>arr[i];
+    int maxLength = 0;
+    int currLength = 0;
+    int curr = 0;
+    for(int i=1;i<=n;i++){
+        if(curr < arr[i]){
+            currLength++;
+        }
+        else{
+            maxLength = max(maxLength,currLength);
+            currLength = 1;
+        }
+        curr = arr[i];
+    }
+    cout<<max(maxLength,currLength)<<endl;
+}
