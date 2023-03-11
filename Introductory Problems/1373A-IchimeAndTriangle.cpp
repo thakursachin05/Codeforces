5a81d77202b9206db34bb17462c94bab6eaddf2a
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2023-03-11 23:12:31 +0530

    Added Code

---
+++ Introductory Problems/1373A-IchimeAndTriangle.cpp
@@ -0,0 +1,14 @@
+// https://codeforces.com/problemset/problem/1337/A
+
+#include <iostream>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+
+        cout<<b<<" "<<c<<" "<<c<<endl;
+    }
+}
