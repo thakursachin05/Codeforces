8d092edd28c786a0a94d72bebf7ad3c9dd609f16
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2023-03-13 23:00:49 +0530

    Added Code

---
+++ Maths Propblems/959A-MohumdAndEhanb.cpp
@@ -0,0 +1,13 @@
+// https://codeforces.com/problemset/problem/959/A
+#include <iostream>
+using namespace std;
+int main() {
+    int t = 1;
+    // cin>>t;
+    while(t--){
+        int a;
+        cin>>a;
+        if(a&1) cout<<"Ehab"<<endl;
+        else cout<<"Mahmoud"<<endl;
+    }
+}
