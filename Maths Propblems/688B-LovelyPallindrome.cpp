d16b65eec4a2b5bfb28d43afa6ad4f7006fa820f
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2023-04-27 23:55:35 +0530

    Added Code

---
+++ Maths Propblems/688B-LovelyPallindrome.cpp
@@ -0,0 +1,13 @@
+// https://codeforces.com/problemset/problem/688/B
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	string s;
+    cin>>s;
+    cout<<s;
+    reverse(s.begin(),s.end());
+    cout<<s<<endl;
+}
