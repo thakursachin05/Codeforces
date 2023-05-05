d81bf00eedbf8659aae612ab1a2a65b83297ec19
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2023-05-05 23:09:40 +0530

    Added Code

---
+++ Maths Propblems/630C-LuckyNumber.cpp
@@ -0,0 +1,12 @@
+// https://codeforces.com/problemset/problem/630/C
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    // cout<<"Hello World!";
+    int n;
+    cin>>n;
+    // n++;
+    unsigned long long ans = (2LL << n) - 2;
+    cout<<ans<<endl;
+}
