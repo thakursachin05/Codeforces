00d34b7679239439ae899a7a4b475e0475a7dff4
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2023-06-06 21:15:13 +0530

    Added Code

---
+++ Bit Manupulation/1125C-pBinary.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(){
+    int n,p,i=1;
+    scanf("%d%d",&n,&p);
+    for(;i<=30;++i)if(n-i*p>=i&&__builtin_popcount(n-i*p)<=i)return 0*printf("%d\n",i);
+    return 0*printf("-1");
+}
