da3cba1d0b544b482716b75c812fdf21b1f5af40
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2023-06-26 22:37:45 +0530

    Added Code

---
+++ String Algorithms/String Hashing/7D-PalindromeString.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+const int N=6e6,M=107;
+int a[5000005],l,r,t,i,p=1;
+char s[5000005];
+int main() {
+for(gets(s);s[i];i++)l=l*M+s[i],r+=s[i]*p,p*=M,a[i+1]=(l==r)?a[(i+1)/2]+1:0,t+=a[i+1];
+printf("%d\n",t);
+}
