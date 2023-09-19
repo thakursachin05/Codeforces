6f93b18b85d63206eb3b7d89535b30bb0996300a
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2023-09-19 23:15:56 +0530

    Added Code

---
+++ Dynamic Programming/319C - Kalila and Dimna in the Logging Industry.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+const int N = 100003;
+int n,a[N],b[N],q[N]; long long f[N];
+main(){
+auto S = [](int i,int j){ return (double)(f[i]-f[j])/(b[j]-b[i]); };
+
+scanf("%d",&n);
+for(int i=1;i<=n;++i) scanf("%d",a+i);
+for(int i=1;i<=n;++i) scanf("%d",b+i);
+int l=1,r=1; q[1]=1;
+for(int i=2;i<=n;++i){
+while(l<r&&S(q[l+1],q[l])<=a[i]) ++l;
+f[i]=f[q[l]]+1ll*b[q[l]]*a[i];
+while(l<r&&S(q[r],q[r-1])>=S(i,q[r])) --r; q[++r]=i;
+} printf("%lld",f[n]);
+}
